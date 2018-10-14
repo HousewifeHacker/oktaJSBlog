3715c8e231f6921d53d7dc78c785f51111f75182
housewifehacker <housewifehacker@gmail.com>
2018-10-14 15:57:42 -0500

    Set up express API with crud endpoints for posts only for authorized users

---
+++ src/server/index.js
@@ -0,0 +1,58 @@
+// use env from frontend
+require('dotenv').config({ path: '.env.local' });
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+const Sequelize = require('sequelize');
+const epilogue = require('epilogue');
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+    clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+    issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+});
+
+
+const app = express();
+// cross origin and json parsing
+app.use(cors());
+app.use(bodyParser.json());
+
+// before request, route gate keeper checks if authorized
+app.use(async (req, res, next) => {
+    try {
+        if (!req.headers.authorization) throw new Error('Authorization header is required');
+
+        const accessToken = req.headers.authorization.trim().split(' ')[1];
+        await oktaJwtVerifier.verifyAccessToken(accessToken);
+        next();
+    } catch (error) {
+        next(error.message);
+    }
+});
+
+//sqlite db of posts
+const database = new Sequelize({
+    dialect: 'sqlite',
+    storage: './test.sqlite',
+});
+const Post = database.define('posts', {
+    title: Sequelize.STRING,
+    body: Sequelize.TEXT,
+});
+
+// give us CRUD methods for posts
+epilogue.initialize({ app, sequelize: database });
+epilogue.resource({
+    model: Post,
+    endpoints: ['/posts', '/posts/:id'],
+});
+
+const port = process.env.SERVER_PORT || 3001;
+
+database.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    });
+});
